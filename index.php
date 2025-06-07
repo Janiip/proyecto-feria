cc15e1b73e4a6d4dc4b8a95ca4367756fffef587
Janiip <168372737+Janiip@users.noreply.github.com>
2025-06-06 21:31:01 -0300

    cambios CSS
    Estructura barra nav;
    Inicio, navegación de alumno y personal + animaciones básicas

--- index.php
+++ index.php
@@ -4,25 +4,24 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inicio</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
     <div id="boton-seleccion">
         <h1>Bienvenido a Asistencia digital</h1>
         <h2>Seleccione que formulario quiere ver</h2>
         <div id="ver-lista">
-            <h3>Ver lista cargada</h3>
-            <button><a href="listado.php">Ver</a></button>
+            <a href="listado.php">Ver lista cargada</a>
         </div>
         <h2>Cargar datos</h2>
-        <div>
-            <button><a href="alumno.php">Alumnado</a></button>
+        <div id="alumno-b">
+            <a href="alumno.php">Alumnado</a>
         </div>
-        <div>
-            <button><a href="personal.php">Personal</a></button>
+        <div id="personal-b">
+            <a href="personal.php">Personal</a>
         </div>
-        <div>
-            <button><a href="escanear_asistencia.php">Entrada</a></button>
+        <div id="entrada-b">
+            <a href="escanear_asistencia.php">Entrada</a>
         </div>
     </div>
 </body>
--- lista-personal.php
+++ lista-personal.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lista de personal</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
     <div id="barra-nav-pers">
@@ -29,6 +29,7 @@
 </html>
 
 <?php
+
 include "conexion.php";
 $query = "SELECT * FROM `personal`";
 $res = mysqli_query($con, $query);
