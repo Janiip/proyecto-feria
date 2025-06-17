5e3cb8b97ee0189566c52557e4717ea2e3f81c63
X-forex-X <138679546+X-forex-X@users.noreply.github.com>
2025-06-17 19:12:21 -0300

    Boton delete
    Se agrego el boton delete a los listados de alumno y personal

--- lista-alumno.php
+++ lista-alumno.php
@@ -56,12 +56,10 @@
 
 <?php
 include "conexion.php";
-// Obtener filtros del formulario
 $nombre = strtolower(isset($_POST['nombre']) ? $_POST['nombre'] : '');
 $curso = strtolower(isset($_POST['curso']) ? $_POST['curso'] : '');
 $division = strtolower(isset($_POST['division']) ? $_POST['division'] : '');
 
-// Construir la consulta con filtros
 if (!empty($nombre) || !empty($curso) || !empty($division)){
         $query = "SELECT * FROM `alumnos` WHERE 1=1";
         if (!empty($nombre)) {
@@ -79,23 +77,29 @@ if (!empty($nombre) || !empty($curso) || !empty($division)){
         echo '<table border="1">';
         echo '<tr><th>Nombre</th><th>Apellido</th><th>DNI</th><th>Año</th><th>División</th><th>Fecha</th><th>UID RFID</th><th>Acción</th></tr>';
 
-        while ($row = mysqli_fetch_array($res)) {
-            echo "<tr>
-                    <form method='POST' action='asignar_uid.php'>
-                        <td>{$row["nombre"]}</td>
-                        <td>{$row["apellido"]}</td>
-                        <td>{$row["dni"]}</td>
-                        <td>{$row["ano"]}</td>
-                        <td>{$row["division"]}</td>
-                        <td>{$row["fecha"]}</td>
-                        <td>
-                            <input type='text' name='uid' value='{$row["rfid_uid"]}' placeholder='Escaneá aquí' required>
-                            <input type='hidden' name='id_alumno' value='{$row["dni"]}'>
-                        </td>
-                        <td><button type='submit'>Guardar</button></td>
-                    </form>
-                </tr>";
-        }
+while ($row = mysqli_fetch_array($res)) {
+    echo "<tr>
+            <form method='POST' action='asignar_uid.php'>
+                <td>{$row["nombre"]}</td>
+                <td>{$row["apellido"]}</td>
+                <td>{$row["dni"]}</td>
+                <td>{$row["ano"]}</td>
+                <td>{$row["division"]}</td>
+                <td>{$row["fecha"]}</td>
+                <td>
+                    <input type='text' name='uid' value='{$row["rfid_uid"]}' placeholder='Escaneá aquí' required>
+                    <input type='hidden' name='id_alumno' value='{$row["dni"]}'>
+                </td>
+                <td>
+                    <button type='submit'>Guardar</button>
+                    <a href='eliminar.php?tipo=alumno&id={$row["dni"]}' onclick=\"return confirm('¿Seguro que deseas eliminar este alumno?');\">
+                        <button type='button' style='background-color:red; color:white;'>Eliminar</button>
+                    </a>
+                </td>
+            </form>
+          </tr>";
+}
+
         echo '</table>';
 }
 
