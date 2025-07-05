49350134d3f78447714425040296576599f41211
Janiip <168372737+Janiip@users.noreply.github.com>
2025-07-04 21:02:18 -0300

    CAMBIOS (sigue sin andar)
    bronca

---
+++ login.php
@@ -0,0 +1,22 @@
+<?php
+include "conexion.php";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $usuario=$_POST['usuario'];
+    $clave=$_POST['clave'];
+    $stmt = $con ->prepare("SELECT usuario FROM login WHERE usuario=? ");
+    $stmt->bind_param("s", $usuario);
+    $stmt->execute();
+    $resultado = $stmt->get_result();
+
+    if($fila = $resultado ->fetch_assoc()){
+        // Redirige según el rol
+        if($usuario == 'admin' && $clave == 'admin123'){
+            header("Location: inicio-admin.html");
+        }
+        exit();
+    }else {
+    echo "Usuario o clave incorrectos";
+    }
+}
+?>
