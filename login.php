6f9c0456512fd7ed9d5fe7bd1c7d65bfec1525f0
ericote22334 <rothsepulvedaeric@gmail.com>
2025-08-07 18:39:29 -0300

    CAMBIOS
    se arreglo el login

--- login.php
+++ login.php
@@ -4,13 +4,6 @@ include "conexion.php";
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $usuario=$_POST['usuario'];
     $clave=$_POST['clave'];
-    $stmt = $con ->prepare("SELECT usuario FROM login WHERE usuario=? ");
-    $stmt->bind_param("s", $usuario);
-    $stmt->execute();
-    $resultado = $stmt->get_result();
-   
-    if($fila = $resultado ->fetch_assoc()){
-        // Redirige según el rol
         if($usuario == 'admin' && $clave == 'admin123'){
             header("Location: inicio-admin.html");
         }
@@ -21,5 +14,5 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }else {
     echo "Usuario o clave incorrectos";
     }
-}
 ?>
+
