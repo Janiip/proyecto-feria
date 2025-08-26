74c409e471889cd8dc3a8d0892ac672d816d3fd3
Janiip <168372737+Janiip@users.noreply.github.com>
2025-08-26 18:41:05 -0300

    cambios
    ni idea

--- cargar-autorizados.php
+++ cargar-autorizados.php
@@ -11,17 +11,29 @@
         <div id="titulo">
             <h2>Datos del/la autorizado/a</h2>    
         </div>
-            
-                <span>Nombre/s del/la autorizado/a: </span>
+                <span>Nombre/s del/la autorizado/s: </span>
                 <input type="text" name="nombre-al" placeholder="Ingrese nombre/s">
             </label>
             <label>
-                <span>Apellido/s del/la autorizado/a: </span>
+                <span>Apellido/s del/la autorizado/s: </span>
                 <input type="text" name="ape-al" placeholder="Ingrese apellido/s">
             </label>
-             <span>Número de documento del/la autorizado/a: </span>
+             <span>Número de documento del/la autorizado/s: </span>
                 <input type="number" name="dni-al" placeholder="Solo números" min=0>
             </label>
+            <label>
+                <span>Parentezco del/la autorizado/s:</span>
+                <input type="text" name="parentesco-al" placeholder="Ingrese parentesco">
+            </label>
+            <label>
+                <span>Alumno autorizado:</span>
+                <input type="text" name="autorizado-al" placeholder="Ingrese el nombre y apellido">
+            </label>
+            <label>
+                <span>Curso y división del alumno autorizado:</span>
+                <input type="text" name="curso-al" placeholder="Ej: 1d">
+            </label>
+            
            <div class="btn-enviar">
                 <button type="submit" name="submit">Enviar</button>
             </div>
---
+++ config.php
@@ -0,0 +1,4 @@
+<?php
+return [
+    'api_key' => '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'  // ⚠️ Reemplazá con tu clave real
+];
